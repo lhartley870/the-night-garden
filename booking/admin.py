1c67fece2c36c7a4ad9ddbc7778ae18406526eeb
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-12 20:53:52 +0000

    Register Table model & Admin class with admin site

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Table
+
 
 # Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'size')
+    list_filter = ('name', 'size')
+    search_fields = ['name', 'size']

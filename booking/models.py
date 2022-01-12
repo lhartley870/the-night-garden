c56217923dffd173f9a39df1373c2fe50adf51e0
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-12 20:49:37 +0000

    Create Table model & migrate to Postgres database

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Table(models.Model):
+    SIZE = [(2, 2), (4, 4), (6, 6), (8, 8)]
+    name = models.CharField(max_length=15, unique=True)
+    size = models.PositiveSmallIntegerField(choices=SIZE)
+
+    class Meta:
+        ordering = ['size']
+
+    def __str__(self):
+        return self.name

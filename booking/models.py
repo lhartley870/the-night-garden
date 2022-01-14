99bc8d17706d692dc0dbb857954ab64dc71e83c0
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-14 15:28:18 +0000

    Create TimeSlot model, migrate to Postgres database

--- booking/models.py
+++ booking/models.py
@@ -12,3 +12,14 @@ class Table(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TimeSlot(models.Model):
+    time = models.TimeField(unique=True)
+    tables = models.ManyToManyField(Table, related_name='table_timeslots')
+
+    class Meta:
+        ordering = ['time']
+
+    def __str__(self):
+        return self.time.strftime("%H:%M")

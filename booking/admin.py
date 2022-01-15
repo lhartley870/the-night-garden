85c096a711605629e0505ff4893ac0f0d8186e0b
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-15 16:52:05 +0000

    Move allocated_tables method into a Mixin class

--- booking/admin.py
+++ booking/admin.py
@@ -3,6 +3,28 @@ from .models import Table, TimeSlot
 from .forms import TimeSlotForm
 
 
+class TablesMixin:
+    """
+    Mixin to allow table details to be displayed in an Admin panel list_display
+    where tables is a Many to Many field and so not supported in list_display.
+    """
+    def allocated_tables(self, obj):
+        """
+        Method to enable the name and size of each allocated table to be
+        displayed in an Admin panel list.
+        """
+        # Code for the display of each table's details was adapted
+        # from an answer given by  karthikr and edited by Joseph jun.
+        # Melettukunnel on this Stack Overflow post -
+        # https://stackoverflow.com/questions/18108521/many-to-many-in-list-display-django
+        return ", ".join(
+            [
+                f'{table.name} ({table.size})'
+                for table in obj.tables.all()
+            ]
+        )
+
+
 # Register your models here.
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
@@ -13,7 +35,7 @@ class TableAdmin(admin.ModelAdmin):
 
 
 @admin.register(TimeSlot)
-class TimeSlotAdmin(admin.ModelAdmin):
+class TimeSlotAdmin(admin.ModelAdmin, TablesMixin):
 
     form = TimeSlotForm
 
@@ -26,20 +48,7 @@ class TimeSlotAdmin(admin.ModelAdmin):
         """
         Method to display each time slot in the Admin panel
         in the 24 hour clock format rather than the default '5:30 p.m.' format
-        for consistency with the database and the search field.
+        for consistency with the database and the search field format.
         """
         twenty_four_hour_clock_time = obj.time.strftime("%H:%M")
         return twenty_four_hour_clock_time
-
-    def allocated_tables(self, obj):
-        """
-        Method to enable the name and size of each table allocated to a
-        time slot to be displayed in the Admin panel list of time slots as
-        Many to Many fields are not supported in list_display.
-        """
-        # Code for the display of each time slot's table details was adapted
-        # from an answer given by  karthikr and edited by Joseph jun.
-        # Melettukunnel on this Stack Overflow post -
-        # https://stackoverflow.com/questions/18108521/many-to-many-in-list-display-django
-        return ", ".join([f'{table.name} ({table.size})'
-                         for table in obj.tables.all()])

6527b8c6263495b46e054b555a24fc0607544817
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-20 13:04:51 +0000

    Add BookingFormPage view & get method

--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import BookingForm
 
 
 # Create your views here.
 class HomePage(View):
     def get(self, request):
         return render(request, "index.html",)
+
+
+class BookingFormPage(View):
+    def get(self, request):
+        return render(
+            request,
+            "booking_form.html",
+            {
+                "booking_form": BookingForm(),
+            }
+        )

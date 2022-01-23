9e2b570e787340628b994c950d4cf8fcfd4d593f
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-23 15:20:54 +0000

    Add select_tables method in BookingFormPg view

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import BookingForm
+from .models import Table
+import random
 
 
 # Create your views here.
@@ -19,9 +21,33 @@ class BookingFormPage(View):
             }
         )
 
+    def select_tables(self, booking_form):
+        time_slot = booking_form.cleaned_data['time_slot']
+        date = booking_form.cleaned_data['date']
+        party_size = booking_form.cleaned_data['party_size']
+        bookings = booking_form.get_current_bookings(date, time_slot)
+
+        available_tables = Table.objects.filter(
+            table_timeslots=time_slot
+        ).exclude(
+            table_booking__in=bookings
+        )
+
+        if len(available_tables) == 1:
+            allocated_tables = available_tables[0]
+        elif len(available_tables) > 1:
+            allocated_tables = []
+        else:
+            allocated_tables = None
+
+        return allocated_tables
+
     def post(self, request, *args, **kwargs):
 
         booking_form = BookingForm(data=request.POST)
+
+        if booking_form.is_valid():
+            allocated_tables = self.select_tables(booking_form)
 
         return render(
             request,

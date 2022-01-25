60d9af74c29881ad3a46db89a87b4d7a4adb089f
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-25 19:57:07 +0000

    Add comments to BookingFormPg select_tables method

--- booking/views.py
+++ booking/views.py
@@ -37,22 +37,43 @@ class BookingFormPage(View):
         return allocated_tables
 
     def select_tables(self, booking_form):
+        """
+        Method to select the table(s) for the booking.
+
+        Gets all tables assigned to the chosen time_slot and removes any
+        that have already been booked on the same date and for the same
+        time slot. If only 1 table is available, that is returned. If more
+        than 1 table is available, the evaluate_multiple tables method is
+        called to return the table(s). In the unlikely event that no tables
+        are available, 'None' is returned.
+
+        This method provides the selected tables to the post method.
+        """
         time_slot = booking_form.cleaned_data['time_slot']
         date = booking_form.cleaned_data['date']
         party_size = booking_form.cleaned_data['party_size']
         bookings = booking_form.get_current_bookings(date, time_slot)
 
+        # Gets all the tables assigned to the chosen time slot and
+        # removes any tables that have already been booked on the same date
+        # and for that same time slot.
         available_tables = Table.objects.filter(
             table_timeslots=time_slot
         ).exclude(
             table_booking__in=bookings
         )
 
+        # If there is only 1 available table, that is returned.
         if len(available_tables) == 1:
             allocated_tables = available_tables
+        # If there is more than 1 available table, the
+        # evaluate_multiple_tables method is called.
         elif len(available_tables) > 1:
             allocated_tables = self.evaluate_multiple_tables(available_tables,
                                                              party_size)
+        # There should be tables available due to the validation carried out
+        # prior to reaching this point but in the unlikely event that no tables
+        # are available, 'None' is returned.
         else:
             allocated_tables = None
 

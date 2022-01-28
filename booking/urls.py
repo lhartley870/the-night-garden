0d34933df855b4e0075e7fae5d9ecb80cf1aa092
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-28 23:01:23 +0000

    Add my bookings page path to booking app urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('make-booking', views.BookingFormPage.as_view(), name='make_booking'),
+    path('my-bookings', views.MyBookingsPg.as_view(), name='my_bookings'),
 ]

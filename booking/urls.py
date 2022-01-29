5d8236352568d15e1ada848a121865abdb0029b5
lhartley870 <hartleylc@hotmail.co.uk>
2022-01-29 16:39:32 +0000

    Add login_required decorator to url paths

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('make-booking', views.BookingFormPage.as_view(), name='make_booking'),
-    path('my-bookings', views.MyBookingsPg.as_view(), name='my_bookings'),
+    # The solution of using the login_required decorator to control what
+    # happens if a user logs out of their account and then presses the
+    # back button was taken from an answer given by Mahmood on this Stack
+    # Overflow post -
+    # https://stackoverflow.com/questions/28000981/django-user-re-entering
+    # -session-by-clicking-browser-back-button-after-logging?noredirect=1&lq=1
+    #
+    # The solution of using the login_required decorator in the url path
+    # for class-based views was taken from an answer given by FMZ on this
+    # Stack Overflow post -
+    # https://stackoverflow.com/questions/28555260/django-login-required-for-class-views
+    path('make-booking', login_required(views.BookingFormPage.as_view()),
+         name='make_booking'),
+    path('my-bookings', login_required(views.MyBookingsPg.as_view()),
+         name='my_bookings'),
 ]

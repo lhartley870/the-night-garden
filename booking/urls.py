5e341efd472eeea01bdce747674dd45001ed385c
lhartley870 <hartleylc@hotmail.co.uk>
2022-02-13 20:45:39 +0000

    Rename views in paths in booking app urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
+    path('', views.Home.as_view(), name='home'),
     # The solution of using the login_required decorator to control what
     # happens if a user logs out of their account and then presses the
     # back button was taken from an answer given by Mahmood on this Stack
@@ -15,12 +15,12 @@ urlpatterns = [
     # for class-based views was taken from an answer given by FMZ on this
     # Stack Overflow post -
     # https://stackoverflow.com/questions/28555260/django-login-required-for-class-views
-    path('make-booking', login_required(views.BookingFormPage.as_view()),
+    path('make-booking', login_required(views.MakeBooking.as_view()),
          name='make_booking'),
-    path('my-bookings', login_required(views.MyBookingsPg.as_view()),
+    path('my-bookings', login_required(views.MyBookings.as_view()),
          name='my_bookings'),
     path('edit-booking/<int:booking_id>',
-         login_required(views.EditBookingPage.as_view()),
+         login_required(views.EditBooking.as_view()),
          name='edit_booking'),
     path('cancel-booking/<int:booking_id>',
          views.CancelBooking.as_view(),

f7f18f9bc68bdb3080edeb2254b9cbcacde09f83
lhartley870 <hartleylc@hotmail.co.uk>
2022-02-20 15:00:38 +0000

    Convert table names to lowercase using save method

--- booking/models.py
+++ booking/models.py
@@ -4,29 +4,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
-class NameField(models.CharField):
-    """
-    Special NameField class added to be used for name fields
-    so that they are converted to lowercase characters before
-    being entered in the database. This prevents the same names
-    with different uppercase and lowercase characters from getting
-    around any requirement for the name field to be 'unique'.
-    """
-
-    # Code for this NameField class taken from an answer given
-    # by Danil and edited by Oran on this Stack Overflow post -
-    # https://stackoverflow.com/questions/36330677/django-model
-    # -set-default-charfield-in-lowercase
-    def __init__(self, *args, **kwargs):
-        super(NameField, self).__init__(*args, **kwargs)
-
-    def get_prep_value(self, value):
-        return str(value).lower()
-
-
 class Table(models.Model):
     SIZE = [(2, 2), (4, 4), (6, 6), (8, 8)]
-    name = NameField(max_length=15, unique=True)
+    name = models.CharField(max_length=15, unique=True)
     size = models.PositiveSmallIntegerField(choices=SIZE)
 
     class Meta:
@@ -34,6 +14,22 @@ class Table(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        """
+        Customised save method added to convert all table names
+        to lowercase characters before being entered in the database.
+        This prevents the same names with different uppercase and
+        lowercase characters from getting around the requirement for
+        the table name field to be 'unique'.
+        """
+        # Code for converting a model field to lowercase before
+        # saving it in the database taken from an answer given
+        # by Mattia on this Stack Overflow post -
+        # https://stackoverflow.com/questions/48574940/how-do-you-
+        # make-a-lowercase-field-in-a-django-model/48596135
+        self.name = self.name.lower()
+        return super(Table, self).save(*args, **kwargs)
 
 
 class TimeSlot(models.Model):

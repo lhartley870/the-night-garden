fc45d6cbb861e87b731cd9844d80843e88ab205a
lhartley870 <hartleylc@hotmail.co.uk>
2022-02-20 19:00:50 +0000

    Add NameField class back in to run migrations

--- booking/models.py
+++ booking/models.py
@@ -4,6 +4,26 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
+class NameField(models.CharField):
+    """
+    Special NameField class added to be used for name fields
+    so that they are converted to lowercase characters before
+    being entered in the database. This prevents the same names
+    with different uppercase and lowercase characters from getting
+    around any requirement for the name field to be 'unique'.
+    """
+
+    # Code for this NameField class taken from an answer given
+    # by Danil and edited by Oran on this Stack Overflow post -
+    # https://stackoverflow.com/questions/36330677/django-model
+    # -set-default-charfield-in-lowercase
+    def __init__(self, *args, **kwargs):
+        super(NameField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        return str(value).lower()
+
+        
 class Table(models.Model):
     SIZE = [(2, 2), (4, 4), (6, 6), (8, 8)]
     name = models.CharField(max_length=15, unique=True)

fd955ef0b7a65e4fc89d7bc0988ce4830a65b653
lhartley870 <hartleylc@hotmail.co.uk>
2022-02-25 16:25:53 +0000

    Add if statement-serving user uploaded media files

--- nightgarden/urls.py
+++ nightgarden/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls'), name='booking_urls'),
     path('accounts/', include('allauth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

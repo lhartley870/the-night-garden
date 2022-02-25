4607d2804320442434fef928cfaa4429e568dc94
lhartley870 <hartleylc@hotmail.co.uk>
2022-02-25 20:10:35 +0000

    Remove if statement for media files

--- nightgarden/urls.py
+++ nightgarden/urls.py
@@ -22,8 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls'), name='booking_urls'),
     path('accounts/', include('allauth.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

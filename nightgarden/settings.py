a0801d9f2e9241693b8de53b2e7438a6e7c5d2fe
lhartley870 <hartleylc@hotmail.co.uk>
2022-02-26 12:57:20 +0000

    Remove settings for using {{MEDIA_URL}} links

--- nightgarden/settings.py
+++ nightgarden/settings.py
@@ -103,7 +103,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',
             ],
         },
     },
@@ -181,7 +180,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

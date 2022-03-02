57f1e44ea1a4cb42df09810783ddd6b6cd7bd6d4
lhartley870 <hartleylc@hotmail.co.uk>
2022-03-02 14:51:07 +0000

    Remove standard django comment from models.py

--- booking/models.py
+++ booking/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-# Create your models here.
 class NameField(models.CharField):
     """
     Special NameField class added to be used for name fields

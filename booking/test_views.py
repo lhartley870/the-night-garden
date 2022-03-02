341b9fafe1290208d3466b7622d674f35fda24a6
lhartley870 <hartleylc@hotmail.co.uk>
2022-03-02 19:40:22 +0000

    Remove unused BookingForm import

--- booking/test_views.py
+++ booking/test_views.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Table, TimeSlot, Booking
-from .forms import BookingForm
 
 
 class TestViews(TestCase):

933ff5c258505a2bfb4f78eaca51906ede3f0c38
lhartley870 <hartleylc@hotmail.co.uk>
2022-11-27 16:17:46 +0000

    Updates for using ElephantSQL database and deployment using Render

--- nightgarden/settings.py
+++ nightgarden/settings.py
@@ -40,6 +40,11 @@ if development:
 else:
     ALLOWED_HOSTS = ['the-night-garden.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 ACCOUNT_EMAIL_REQUIRED = True
